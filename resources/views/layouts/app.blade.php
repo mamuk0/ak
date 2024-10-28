1f2a2f0f8f3139176fcee9d4dbe5f9e9c73051b2
mamuk0 <domuzz@yetimhanekundakcisi44.local>
2024-10-28 14:51:24 +0300

    added meta pixel integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,24 @@
         id="pianjs"
         data-code="Ew8hFhIoWfjjYOn79pL6uxpIZ4pXUDKY"></script>
 
+    <!-- Meta integration -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1078700936618094');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1078700936618094&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta integration -->
+
 </head>
 <body class="bg-gray-100">
     <div class="bg-red-600 py-2">
